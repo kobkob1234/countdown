65afe61db3b010592ad6c07fd75bd9d408632706
kobkob1234 <kobiamit@campus.technion.ac.il>
2026-05-10 19:17:35 +0300

    fix: restrict CORS origin and remove public backup files
    - Security: Changed Access-Control-Allow-Origin from * to https://kobkob1234.github.io in api/remind-again.js to prevent malicious cross-origin requests.
    - Cleanup: Removed large .bak files from public repo to reduce size and prevent leakage of historical code/keys.

--- api/remind-again.js
+++ api/remind-again.js
@@ -20,7 +20,7 @@ function hashKey(input) {
 }
 
 function setCorsHeaders(res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://kobkob1234.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 }
